10ab560a9280597198da5e4f8cc1ea9052a7be10
schintam <schintam@victoria.com>
2024-08-12 17:20:03 -0400

    Revert Hello World Changes

--- components/HelloWorld.tsx
+++ components/HelloWorld.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Document, Reference } from '@bloomreach/spa-sdk';
 import { BrProps } from '@bloomreach/react-sdk';
 
 interface HelloWorldParameters {
-  document?: Reference;
+  title?: string;
 }
 
 export function HelloWorld({ component, page }: BrProps): React.ReactElement | null {
-  const { document: documentRef } = component.getModels<HelloWorldParameters>();
-  const document = documentRef && page.getContent<Document>(documentRef);
-
-  if (!document) {
-    return page.isPreview() ? <div/ > : null;
-  }
-
-  const { title } = document.getData<BannerDocument>();
+  const { title } = component.getParameters<HelloWorldParameters>();
 
   return (
     <div className="mw-container mx-auto my-4">
